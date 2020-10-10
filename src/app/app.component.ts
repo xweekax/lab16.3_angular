64c2bc91a91cd7eac965a3b9017b38fa48c9c83c
james <jamesnofs@quickenloans.com>
2020-10-10 14:26:14 -0400

    mvp met - delete working - edit not functional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'Back To Chorin';
@@ -23,14 +24,22 @@ export class AppComponent {
     this.thingstodo.push({task: newtask, completed: false});
   }
 
+  updateitem(itemedit: string){ //this is meant to edit but will push a new "toDo" if called
+    this.thingstodo.push({task: itemedit, completed: false});
+  }
+
   completeTask(iscompleted: ToDo){
     iscompleted.completed = true;
   }
 
   removeTask(remove: ToDo){
-    this.thingstodo = this.thingstodo.filter(item=>item!==remove);
+    this.thingstodo.splice(this.thingstodo.indexOf(remove),1);
+    /*
+    this.thingstodo = this.thingstodo.filter(item=>{return item!==remove});
     console.log("youve clicked the x");
+    console.log(this.thingstodo);*/
   }
+
 }
 
 interface ToDo{
